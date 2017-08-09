57a94103a2d83b4fff5702c888b9dec688132b60
Aksel Ethem <aksel.ethem@gmail.com>
2017-08-09 13:52:49 +0200

    Added shift option

--- caesar.c
+++ caesar.c
@@ -5,18 +5,18 @@
 #include <unistd.h>
 
 
-char * crypting(char input[], int encrypt) {
+char * crypting(char input[], int encrypt, int shift) {
     int length = strlen(input);
     char *crypted;
     
     crypted = malloc(length+1);
-
+    printf("Shift: %d\n", shift);
     int i;
     for(i=0; i < length; i++) {
         if (encrypt == 1) {
-            crypted[i] = input[i] + 10;
+            crypted[i] = input[i] + shift;
         } else {
-            crypted[i] = input[i] - 10;
+            crypted[i] = input[i] - shift;
         }
     }
 
@@ -30,12 +30,15 @@ int main(int argc, char **argv) {
     int eflag = 0;
     int dflag = 0;
     int t;
+    int s;
     char *tvalue = NULL;
+    char *svalue = NULL;
+
     int fflag = 0;
     int c;
     opterr = 0;
 
-    while ((c=getopt(argc, argv, "edft:")) != -1) {
+    while ((c=getopt(argc, argv, "edft:s:")) != -1) {
         switch(c) {
             case 'e':
                 eflag = 1;
@@ -46,11 +49,14 @@ int main(int argc, char **argv) {
             case 't':
                 tvalue = optarg;
                 break;
+            case 's':
+                svalue = optarg;
+                break;
             case 'f':
                 fflag = 1;
                 break;
             case '?':
-                if (optopt == 't') {
+                if (optopt == 't' || optopt == 's') {
                     fprintf(stderr, "Option -%c requires an argument.\n", optopt);
                 } else if (isprint(optopt)) {
                     fprintf(stderr, "Unknown option '-%c'.\n", optopt);
@@ -74,15 +80,21 @@ int main(int argc, char **argv) {
         printf("Please provide the correct arguments. Use 'man caeser' for options.\n");
         exit(1);
     }
+    
+    int shift = 10;
+    if (svalue) {
+        shift = atoi(svalue);
+    }
 
     if (tvalue) {
+
         if (dflag) {
-            char *result = crypting(tvalue, 0);
+            char *result = crypting(tvalue, 0, shift);
             printf("%s\n", result);
         }
 
         if (eflag) {
-            char *result = crypting(tvalue, 1);
+            char *result = crypting(tvalue, 1, shift);
             printf("%s\n", result);
         }
         return 0;
